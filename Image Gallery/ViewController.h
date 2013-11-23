2d40ecd70f4d748c16bc3f5c6920942eddfa9038
Yi Wang <yiwang1982@Yis-MacBook-Air.local>
2013-11-22 23:43:57 -0500

    Initial Commit

---
+++ Image Gallery/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Image Gallery
+//
+//  Created by Yi Wang on 11/22/13.
+//  Copyright (c) 2013 Yi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
